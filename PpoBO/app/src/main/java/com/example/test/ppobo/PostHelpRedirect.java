ab11ae31bf55ac73bba24be5f2bd4232b2bd67fd
Darian Chen <35019741+DarianChen@users.noreply.github.com>
2020-01-19 03:58:19 -0700

    design 1: the prequel

--- PpoBO/app/src/main/java/com/example/test/ppobo/PostHelpRedirect.java
+++ PpoBO/app/src/main/java/com/example/test/ppobo/PostHelpRedirect.java
@@ -3,6 +3,9 @@ package com.example.test.ppobo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class PostHelpRedirect extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class PostHelpRedirect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_help_redirect);
+        RunAnimation();
+    }
+
+    private void RunAnimation()
+    {
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.scale);
+        a.reset();
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        tv.clearAnimation();
+        tv.startAnimation(a);
     }
 }
