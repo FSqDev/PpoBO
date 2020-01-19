3d4c50cfcf0c054755a72250d4912389fae57a63
AnmolDhaliwal <AnmolDhaliwal00@users.noreply.github.com>
2020-01-19 03:13:15 -0700

    Post screen provided after a care receiver asks for help

---
+++ PpoBO/app/src/main/java/com/example/test/ppobo/PostHelpRedirect.java
@@ -0,0 +1,14 @@
+package com.example.test.ppobo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostHelpRedirect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_help_redirect);
+    }
+}
