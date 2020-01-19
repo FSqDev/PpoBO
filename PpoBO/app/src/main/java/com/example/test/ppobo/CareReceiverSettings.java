c7c0c763531256ba00573f361b47f6c28b3ed716
rqin1 <rqin1@ualberta.ca>
2020-01-18 17:51:27 -0700

    set up firebase

--- PpoBO/app/src/main/java/com/example/test/ppobo/CareReceiverSettings.java
+++ PpoBO/app/src/main/java/com/example/test/ppobo/CareReceiverSettings.java
@@ -2,7 +2,9 @@ package com.example.test.ppobo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CareReceiverSettings extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class CareReceiverSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_care_receiver_settings);
+
+        findViewById(R.id.manage_caretakers).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CareReceiverSettings.this,ManageCareTakers.class);
+                startActivity(intent);
+            }
+        });
     }
 }
